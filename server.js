2ada30bdce5e75a5aeed69f01af270aa6ebda188
nv22132 <150009692+nv22132@users.noreply.github.com>
2024-04-15 09:33:25 +0000

    connecting node.js

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-import  express  from "express";
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db";
 
- import dotenv from "dotenv";
+dotenv.config();
+connectDB();
+const app = express();
+app.use(express.json());
 
- dotenv.config();
- const app = express();
- app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Server is running...");
+});
 
- app.get('/',(req,res) => {
-    res.send("server is running...")
- });
+const PORT = process.env.PORT || 5000;
 
- const PORT = process.env.PORT || 5000; 
-
- app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
